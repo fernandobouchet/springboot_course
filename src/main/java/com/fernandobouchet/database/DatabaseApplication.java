9e2978a8a3ec3fc34b80608716b8552acd16a077
fernandobouchet <fernandobouchet@gmail.com>
2024-02-05 19:47:20 -0300

    feat: JDBC Template setup

--- src/main/java/com/fernandobouchet/database/DatabaseApplication.java
+++ src/main/java/com/fernandobouchet/database/DatabaseApplication.java
@@ -1,31 +1,13 @@
 package com.fernandobouchet.database;
 
 import lombok.extern.java.Log;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 @Log
-public class DatabaseApplication implements CommandLineRunner {
-	private final DataSource dataSource;
-
-    public DatabaseApplication(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
+public class DatabaseApplication  {
     public static void main(String[] args) {
 		SpringApplication.run(DatabaseApplication.class, args);
-	}
-
-	@Override
-			public void run(final String... args) {
-		log.info("Datasource: " + dataSource.toString());
-		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
-		restTemplate.execute("select 1");
-
 	}
 }
