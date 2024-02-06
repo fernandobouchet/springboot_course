ec2bc9b8bb90be6746046cfdd9e13dee3fa53208
fernandobouchet <fernandobouchet@gmail.com>
2024-02-05 23:16:18 -0300

    feat: Initial setup for testing

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,17 @@
 DROP TABLE IF EXISTS "books";
 DROP TABLE IF EXISTS "authors";
 
-CREATE TABLE "authors" {
-    "id" bigint DEFAULT nextval(`authors_id_seq`) NOT NULL,
+CREATE TABLE "authors" (
+    "id" bigint DEFAULT nextval('authors_id_seq') NOT NULL,
     "name" text,
     "age" Integer,
     CONSTRAINT "authors_pkey" PRIMARY KEY ("id")
-};
+);
 
-CREATE TABLE "books" {
+CREATE TABLE "books" (
     "isbn" text NOT NULL,
     "title" text,
-    "author_id" bigint,
+    "author_id" bigint NOT NULL,
     CONSTRAINT "books_pkey" PRIMARY KEY ("isbn"),
-    CONSTRAINT "fk_author" FOREIGN KEY (author_id) REFERENCES authors(id)
-};
+    CONSTRAINT "fk_author" FOREIGN KEY ("author_id") REFERENCES "authors"("id")
+);
